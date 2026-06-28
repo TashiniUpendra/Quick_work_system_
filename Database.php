76bcb60bdb568bc02b0ea5f398ecd99954d60882
Tashini Upendra <tashiniupendra@gmail.com>
2026-06-28 09:54:37 +0530

    Add_Database.php

---
+++ Database.php
@@ -0,0 +1,35 @@
+<?php
+
+class Database {
+
+    private $host = "localhost";
+    private $db_name = "quickworksdb";
+    private $username = "root";
+    private $password = "";
+
+    public $conn;
+
+    public function connect() {
+
+        $this->conn = null;
+
+        try {
+            $this->conn = new mysqli(
+                $this->host,
+                $this->username,
+                $this->password,
+                $this->db_name
+            );
+
+            if ($this->conn->connect_error) {
+                die("DB Connection Failed: " . $this->conn->connect_error);
+            }
+
+        } catch (Exception $e) {
+            die("Error: " . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
+}
+?>
