ca9bfcfb04c29357c734d189eddda9be8272f242
Tashini Upendra <tashiniupendra@gmail.com>
2026-07-14 08:28:28 +0530

    Add_admin.js

---
+++ admin.js
@@ -0,0 +1,11 @@
+console.log("Admin Dashboard Loaded 🚀");
+
+// confirm approve
+function confirmApprove() {
+    return confirm("Are you sure you want to approve this user?");
+}
+
+// confirm block
+function confirmBlock() {
+    return confirm("Are you sure you want to block this user?");
+}
